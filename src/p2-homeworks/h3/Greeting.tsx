52b2121a11ded4788dc91ca28becb342bec1401c
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-11-25 23:48:38 +0300

    HW3 without css

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
+    name: string // need to fix any
     setNameCallback: any // need to fix any
-    addUser: any // need to fix any
+    addUser: (name: string) => void // need to fix any
     error: any // need to fix any
-    totalUsers: any // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
@@ -14,12 +14,11 @@ const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
     const inputClass = s.error // need to fix with (?:)
-
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
+            <input value={name} onChange={(e) => setNameCallback(e.currentTarget.value)} className={inputClass}/>
             <span>{error}</span>
-            <button onClick={addUser}>add</button>
+            <button onClick={() => addUser(name)}>add</button>
             <span>{totalUsers}</span>
         </div>
     )
--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,21 +1,25 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v4 as uuidv4} from "uuid";
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string// need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
     const [users, setUsers] = useState<any>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (userName: string) => { // need to fix any
+        const newUsersArray = [...users]
+        newUsersArray.push({name: userName, _id: uuidv4()})
+        setUsers(newUsersArray) // need to fix
     }
-
+    debugger
     return (
+
         <div>
             <hr/>
             homeworks 3
