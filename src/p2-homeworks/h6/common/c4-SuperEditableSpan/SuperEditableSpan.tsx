dd8801e009cbf7a760e6bf5aa94f157f02599e7d
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-01-06 21:38:08 +0300

    HW6 final!

--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -66,7 +66,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
-                        +{children || restProps.value}
+                        &#9998; {children || restProps.value}
                     </span>
                 )
             }
