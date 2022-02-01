53989bdc8f70aefbcb7b3ef57886eb3ec80b8bcf
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-02-01 11:22:52 +0300

    HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import s from "./HW12.module.css";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import {changeThemeAC} from "./bll/themeReducer";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+
+    const activeTheme = useSelector<AppStoreType, string>(state => state.theme.theme)
+    const dispatch = useDispatch()
+
+    const onChangeCallback = (theme: string) => {
+      dispatch(changeThemeAC(theme))
+    }
 
     // useDispatch, onChangeCallback
 
     return (
-        <div className={s[theme]}>
+        <div className={s[activeTheme]}>
             <hr/>
-            <span className={s[theme + '-text']}>
+            <span className={s[activeTheme + `-text`]}>
                 homeworks 12
             </span>
 
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
+            <SuperRadio name={'radio'}
+                        options={themes}
+                        value={activeTheme}
+                        onChangeOption={onChangeCallback}/>
 
             <hr/>
         </div>
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,18 @@
 const initState = {
-
+    theme: 'dark'
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeActionType): typeof initState=> { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "CHANGE-THEME": {
+            return {...state, theme: action.theme};
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeAC = (theme: string) => ({
+    type: 'CHANGE-THEME', theme
+} as const); // fix any
+
+type ChangeThemeActionType = ReturnType<typeof changeThemeAC>
